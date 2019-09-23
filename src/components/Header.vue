ab7d0820197f35dc80320a56f214e8acf7a83883
MichaelZhou <michael.zhou42@gmail.com>
2019-09-23 16:16:33 -0400

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+  <b-nav tabs> <!-- Adds a line underneath the title and counter -->
+    <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
+    <b-nav-item disabled>Counter: 4/10</b-nav-item>
+  </b-nav>
+    </div>
+</template>
