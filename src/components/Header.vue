a14bef0c1ff082d34237e31514dbd7d293d06530
MichaelZhou <michael.zhou42@gmail.com>
2019-09-24 14:08:07 -0400

    answers

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,18 @@
 <template>
     <div>
   <b-nav tabs> <!-- Adds a line underneath the title and counter -->
-    <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
-    <b-nav-item disabled>Counter: 4/10</b-nav-item>
+    <b-nav-item disabled><b>Michael's Quiz App</b></b-nav-item>
+    <b-nav-item disabled>Counter: {{ numCorrect }} / {{ numTotal}} </b-nav-item>
   </b-nav>
     </div>
 </template>
+
+
+<script>
+export default {
+    props: [
+        'numCorrect', // receiving the number of correct and total answers from the parent
+        'numTotal'
+    ]
+}
+</script>
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -2,17 +2,105 @@
   <div>
   <b-jumbotron>
     <template v-slot:lead>
-      Some question here?
+      {{ currentQuestion.question}}
     </template>
 
     <hr class="my-4">
 
-    <p>
-      List of answers
-    </p>
+        <b-list-group>
+        <b-list-group-item
+        v-for="(answer, index) in answers"
+         :key="index"
+         @click="selectAnswer(index)"
+         :class="[selectedIndex === index ? 'selected' : '']"
+        >
+             {{answer}}</b-list-group-item>
+        </b-list-group>
 
-    <b-button variant="primary" href="#">Submit</b-button>
-    <b-button variant="success" href="#">Next</b-button>
+
+    <b-button
+     variant="primary"
+     @click="submitAnswer"
+     >Submit</b-button>
+    <b-button @click="next" variant="success">
+        Next</b-button>
   </b-jumbotron>
   </div>
 </template>
+
+<script>
+import _ from 'lodash'
+export default {
+    props: {
+        currentQuestion: Object,
+        next: Function,
+        increment: Function
+    },
+    data() {
+        return {
+            selectedIndex: null,
+            shuffledAnswers: []
+        }
+        
+    },
+    computed: {
+        answers() {
+            let answers = [...this.currentQuestion.incorrect_answers]
+            answers.push(this.currentQuestion.correct_answer)
+            return answers
+        }
+    },
+    watch: {
+        currentQuestion: {
+            immediate: true, // instead of only running this watch function when current question updates,
+                            // it's going to also run it when currentQuestion first gets passed props and every subsequent
+                            // time it updates its going to also run the handler function
+            handler() {
+            this.selectedIndex = null
+            this.shuffleAnswers()
+            }
+        }
+    },
+    methods: {
+        selectAnswer(index) {
+            console.log(index)
+            this.selectedIndex = index
+        },
+        shuffleAnswers() {
+            let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+            this.shuffledAnswers = _.shuffle(answers)
+        },
+        submitAnswer() {
+            let isCorrect = false
+            if (this.selectedIndex === this.correctIndex) {
+                isCorrect = true
+            }
+        this.increment(isCorrect)
+        }
+    },
+}
+</script>
+
+<style scoped>
+.list-group {
+    margin-bottom: 15px;
+}
+.list-group-item:hover {
+    background: #EEE; 
+    cursor: pointer;
+}
+.btn {
+    margin: 0 5px;
+}
+
+.selected {
+    background-color: lightblue;
+}
+.correct {
+    background-color: lightgreen;
+}
+
+.incorrect {
+    background-color: red;
+}
+</style>
